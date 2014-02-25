e00f18d4bdb5b51c1fdfa771420ba6cd2fc2ab1e
Junyu Wang <junyuwang@Junyus-MacBook-Pro.local>
2014-02-25 09:12:48 -0800

    finished

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -10,17 +10,25 @@ $('#say_hello').click(function() {
 // Problem 2 (Houdini) ------------------------------------------------------
 $('#disappear').click(function() {
   //WRITE CODE HERE
+  $('#houdini_text').hide();
 
 });
 
 $('#reappear').click(function() {
   //WRITE CODE HERE
+  $('#houdini_text').show();
 });
 
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
 // WRITE CODE HERE
+$('#call_me_pink').click(function() {
+	$('#tickled_text').css('color','pink');
+});
 
 
 // Problem 4 (Greet Me) -----------------------------------------------------
 // WRITE CODE HERE
+$('#greet_me').click(function() {
+	alert("Hey, " + $('#my_name').val());
+})
